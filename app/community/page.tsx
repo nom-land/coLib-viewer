903cc349ba4117adda1c0b4b91cebe0a60e12657
Saltad <saltad@proton.me>
2023-08-21 19:21:32 +0800

    feat: use new homepage

---
+++ app/community/page.tsx
@@ -0,0 +1,23 @@
+import { crossbell } from "crossbell/network";
+import Link from "next/link";
+import { homeName } from "../config";
+
+// Only the selected communities will be displayed
+const communityIds = ["57762", "57747", "57798"]; //TODO
+
+export default async function Home() {
+    return (
+        <div className="container mx-auto py-10 px-10 lg:px-40 min-h-screen">
+            <div className="my-10">Welcome to {homeName}!</div>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+                {communityIds.map((id) => (
+                    <Link className="card" key={id} href={`./community/${id}`}>
+                        {id}
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export const revalidate = 60; // revalidate this page every 60 seconds
--- app/page.tsx
+++ app/page.tsx
@@ -13,16 +13,11 @@ if (process.env.CROSSBELL_RPC_ADDRESS === "http://127.0.0.1:8545") {
 
 export default async function Home() {
     return (
-        <div className="container mx-auto py-10 px-10 lg:px-40 min-h-screen">
-            <div className="my-10">Welcome to {homeName}!</div>
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-                {communityIds.map((id) => (
-                    <Link className="card" key={id} href={`./community/${id}`}>
-                        {id}
-                    </Link>
-                ))}
-            </div>
-        </div>
+        <iframe
+            src="https://colib-where-communities-meet.framer.ai/"
+            className="w-full h-screen"
+            sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+        ></iframe>
     );
 }
 
