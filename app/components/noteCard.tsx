76d24c176aa1105f4e0da65f474bb2aca4338ad3
Saltad <saltad@proton.me>
2023-12-06 20:17:17 +0800

    feat: add attachment in notes

--- app/components/noteCard.tsx
+++ app/components/noteCard.tsx
@@ -1,7 +1,9 @@
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
-import { CurationNote } from "../typings/types";
 import CharacterHeader from "./characterHeader";
 import Tags from "./tags";
+import { CurationNote } from "nomland.js";
+import Image from "next/image";
+import { NoteMetadataAttachmentBase } from "crossbell";
 
 // return a component
 export default function NoteCard({
@@ -23,24 +25,37 @@ export default function NoteCard({
                 handle={note.curatorHandle}
                 avatar={note.curatorAvatars[0]}
             />
-            {/* {noteType === "curation" && listIds && (
-                <div className="text-lg my-5">
-                    add it to list{" "}
-                    <span className="text-2xl">
-                        {note.listNames.map((l, i) => (
-                            <Link key={i} href={`/list/${listIds.get(l)}`}>
-                                {l}{" "}
-                                {i !== note.listNames.length - 1 ? ", " : ""}
-                            </Link>
-                        ))}{" "}
-                    </span>
-                </div>
-            )} */}
+
             {note.title.length > 0 && (
                 <div className="text-2xl font-bold my-3">{note.title}</div>
             )}
             <div className="my-3">
                 <ReactMarkdown>{note.content}</ReactMarkdown>
+            </div>
+            <div>
+                {note.attachments?.map(
+                    (
+                        a: NoteMetadataAttachmentBase<"address" | "content">,
+                        i: number
+                    ) =>
+                        a.address && (
+                            <div key={i}>
+                                <Image
+                                    priority
+                                    src={
+                                        a.address?.startsWith("ipfs://")
+                                            ? `https://ipfs.crossbell.io/ipfs/${a.address.slice(
+                                                  7
+                                              )}`
+                                            : a.address
+                                    }
+                                    width={200}
+                                    height={200}
+                                    alt="attachment"
+                                ></Image>
+                            </div>
+                        )
+                )}
             </div>
             <Tags tags={note.suggestedTags}></Tags>
 
